300f7af138ad980cd3d9d21e429fcb8c3f07fd98
James Pacheco <jamesrp13@gmail.com>
2022-12-20 19:06:37 -0700

    Update to game example

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,13 +1,26 @@
-import { LAMPORTS_PER_SOL, Connection, PublicKey } from '@solana/web3.js'
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey"
+import { LAMPORTS_PER_SOL, Connection, PublicKey } from "@solana/web3.js"
 
 export async function safeAirdrop(address: PublicKey, connection: Connection) {
-    const acctInfo = await connection.getAccountInfo(address, "confirmed")
+  const acctInfo = await connection.getAccountInfo(address, "confirmed")
 
-    if (acctInfo == null || acctInfo.lamports < LAMPORTS_PER_SOL) {
-        let signature = await connection.requestAirdrop(
-            address,
-            LAMPORTS_PER_SOL
-        )
-        await connection.confirmTransaction(signature)
-    }
+  if (acctInfo == null || acctInfo.lamports < LAMPORTS_PER_SOL) {
+    let signature = await connection.requestAirdrop(address, LAMPORTS_PER_SOL)
+
+    await connection.confirmTransaction(signature)
+  }
+}
+
+export function getCharacterKey(auth: PublicKey, program: PublicKey) {
+  return findProgramAddressSync([auth.toBuffer()], program)
+}
+
+export function getMetadataKey(
+  auth: PublicKey,
+  gameplayProgram: PublicKey,
+  metadataProgram: PublicKey
+) {
+  const [characterKey] = getCharacterKey(auth, gameplayProgram)
+
+  return findProgramAddressSync([characterKey.toBuffer()], metadataProgram)
 }
